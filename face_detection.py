cf5737b768017dbc0d40224261b394cbfebd4af3
Zulfiqar Ali <117459524+zulfiqarskd17@users.noreply.github.com>
2023-02-02 23:00:51 +0500

    Add files via upload

---
+++ face_detection.py
@@ -0,0 +1,22 @@
+# pip install opencv-python
+# haarcascade_frontalface_dafault.xml
+
+import cv2
+cascade_face = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+cap = cv2.VideoCapture(0) # on the camera
+while True:
+    ret, img = cap.read()
+    print(ret)
+    g = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # covert color into bgr
+    f= cascade_face.detectMultiScale(g, 1.3, 3)
+
+    for(x, y, w, h) in f:
+        cv2.rectangle(img, (x,y),(x + w, y + h ), (0,255,0), 4)
+
+    cv2.imshow('img', img)
+    k = cv2.waitKey(30) & 0xff
+    if k ==27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
